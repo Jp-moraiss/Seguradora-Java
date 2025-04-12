fac075676ed87ed0134c3a95658febddb229be6b
Jp-moraiss <164018039+Jp-moraiss@users.noreply.github.com>
2025-04-11 21:41:22 -0300

    Entidades + Classes DAO + Testes

---
+++ src/br/edu/cs/poo/ac/seguro/entidades/Apolice.java
@@ -0,0 +1,19 @@
+package br.edu.cs.poo.ac.seguro.entidades;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class Apolice implements Serializable{
+
+    private String numero;
+    @NonNull private Veiculo veiculo;
+    @NonNull private BigDecimal valorFranquia;
+    @NonNull private BigDecimal valorPremio;
+    @NonNull private BigDecimal valorMaximoSegurado;
+
+}
---
+++ src/br/edu/cs/poo/ac/seguro/entidades/SeguradoPessoa.java
@@ -0,0 +1,42 @@
+package br.edu.cs.poo.ac.seguro.entidades;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class SeguradoPessoa extends Segurado implements Serializable{
+
+	private String cpf;
+	private double renda;
+	
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public double getRenda() {
+		return renda;
+	}
+	
+	public void setRenda(double renda) {
+		this.renda = renda;
+	}
+	
+	public SeguradoPessoa(String nome, Endereco endereco, LocalDate dataNascimento, BigDecimal bonus,
+			String cpf, double renda) {
+		super(nome, endereco, dataNascimento, bonus);
+		this.cpf = cpf;
+		this.renda = renda;
+	}
+	
+	public LocalDate getDataNascimento() {
+		return super.getDataCriacao();
+	}
+	
+	public void setDataNascimento(LocalDate dataNascimento) {
+		super.setDataCriacao(dataNascimento);
+	}
+}
